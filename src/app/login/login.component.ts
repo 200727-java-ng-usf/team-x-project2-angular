63df564067095245a23ff6449c1da87008392130
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-23 09:46:19 -0400

    added functionality to login component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // Declare Login Form
+  loginForm: FormGroup;
+  // Boolean Loading Value
+  loading = false;
+  // Boolean Submitted Value
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    console.log('LoginComponent instantiating...');
+    console.log('LoginComponent instantiation complete.');
+   }
 
   ngOnInit(): void {
+    console.log('Initializing form values for LoginComponent...');
+
+    // Validate  loginForm
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    console.log('LoginComponent form value initialization complete.');
   }
 
+  get formFields() {
+    return this.loginForm.controls;
+  }
+
+  login() {
+
+    this.submitted = true;
+
+    if (this.loginForm.invalid){
+      return;
+    }
+
+    this.loading = true;
+
+    // TODO: Authenticate using auth service
+  }
 }
