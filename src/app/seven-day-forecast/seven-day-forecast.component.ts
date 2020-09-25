c936e62b73724ba1c38d852ed1907dc5a3e4d403
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-25 12:18:30 -0400

    trying to add functionality to change locations

--- src/app/seven-day-forecast/seven-day-forecast.component.ts
+++ src/app/seven-day-forecast/seven-day-forecast.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SevenDayForecastService } from '../services/seven-day-forecast.service';
-import { ChartOptions, ChartDataSets } from 'chart.js';
+import { ChartOptions, ChartDataSets, Chart } from 'chart.js';
 import { Color, Label, SingleDataSet } from 'ng2-charts';
 import { AuthService } from '../services/auth.service';
 
@@ -12,6 +12,16 @@ import { AuthService } from '../services/auth.service';
 export class SevenDayForecastComponent implements OnInit {
   currentForecast: any = <any>{};
   currentUVForecast: any = <any>{};
+  favoriteLocations = [
+    {
+      locationName: 'sumter, sc',
+      locationZip: 29150
+    },
+    {
+      locationName: 'columbia, sc',
+      locationZip: 29210
+    }
+  ];
   // ------------------- Temperature Chart ----------------------//
   tempChartLabels: Label[] = [];
   tempChartOptions: ChartOptions = {
@@ -104,6 +114,34 @@ export class SevenDayForecastComponent implements OnInit {
 
   }
 
+  async switchLocations(zip: number){
+    this.currentForecast = <Object[]> await this.forecastService.getForecast(zip.toString());
+    this.currentUVForecast = <Object[]> await this.forecastService.getUVForecast(zip.toString());
+    for (let i = 0; i < 40; i++){
+      this.forcastTemps[i] = this.currentForecast.list[i].main.temp;
+    }
+    for (let i = 0; i < 40; i++){
+      this.forecastMaxTemps[i] = this.currentForecast.list[i].main.temp_max;
+    }
+    for (let i = 0; i < 40; i++){
+      this.forecastMinTemps[i] = this.currentForecast.list[i].main.temp_min;
+    }
+    for (let i = 0; i < 40; i++){
+      let str = this.currentForecast.list[i].dt_txt;
+      let subStr = str.substring(5, str.length - 3);
+      this.tempChartLabels[i] = subStr;
+    }
+    for (let i = 0; i < 40; i++){
+      this.forcastHumidity[i] = this.currentForecast.list[i].main.humidity;
+    }
+    // ------------------- UV Chart ----------------------//
+    for (let i = 0; i < 8; i++){
+      this.uvChartLabels[i] = '+' + i;
+    }
+    for (let i = 0; i < 8; i++){
+      this.uvData[i] = this.currentUVForecast[i]?.value;
+    }
+  }
 }
 
 
