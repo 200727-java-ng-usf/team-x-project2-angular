01ebeec1c2d32c92a7ef0009fa13089d0d2f2d79
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-25 23:57:20 -0400

    finished styling forecast component

--- src/app/seven-day-forecast/seven-day-forecast.component.ts
+++ src/app/seven-day-forecast/seven-day-forecast.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { SevenDayForecastService } from '../services/seven-day-forecast.service';
 import { ChartOptions, ChartDataSets, Chart } from 'chart.js';
 import { Color, Label, SingleDataSet } from 'ng2-charts';
@@ -12,136 +12,153 @@ import { AuthService } from '../services/auth.service';
 export class SevenDayForecastComponent implements OnInit {
   currentForecast: any = <any>{};
   currentUVForecast: any = <any>{};
-  favoriteLocations = [
-    {
-      locationName: 'sumter, sc',
-      locationZip: 29150
-    },
-    {
-      locationName: 'columbia, sc',
-      locationZip: 29210
-    }
-  ];
-  // ------------------- Temperature Chart ----------------------//
-  tempChartLabels: Label[] = [];
-  tempChartOptions: ChartOptions = {
-    responsive: true
-  };
-  tempChartColors: Color[] = [
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-    },
-    { // red
-      backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'red',
-    },
-    { // Blue
-      backgroundColor: 'rgba(179,217,255,0.2)',
-      borderColor: 'rgba(51,153,255,1)',
-    }
-  ];
-  tempChartLegend = true;
-  tempChartType = 'line';
-  tempChartPlugins = [];
-  forcastTemps: SingleDataSet = [];
-  forecastMaxTemps: SingleDataSet = [];
-  forecastMinTemps: SingleDataSet = [];
-  tempChartData: ChartDataSets[] = [
-    { data: this.forcastTemps, label: 'Temperature' },
-    { data: this.forecastMaxTemps, label: 'Maximum Temperature' },
-    { data: this.forecastMinTemps, label: 'Minimum Temperature' },
-  ];
-  // ------------------- Humidity Chart ----------------------//
-  forcastHumidity: SingleDataSet = [];
-  humidityChartData: ChartDataSets[] = [
-    { data: this.forcastHumidity, label: 'Humidity' }
-  ];
-  humidityChartColors: Color[] = [
-    { // Blue
-      backgroundColor: 'rgba(179,217,255,0.2)',
-      borderColor: 'rgba(51,153,255,1)',
-    }
-  ];
-  // ------------------- UV Chart ----------------------//
-  uvData: SingleDataSet = [];
-  uvChartData: ChartDataSets[] = [
-    { data: this.uvData, label: 'UV Index' }
-  ];
-  uvChartColors: Color[] = [
-    { // Blue
-      backgroundColor: 'rgba(256,196,59,0.2)',
-      borderColor: 'rgba(252,105,0,1)',
-    }
-  ];
-  uvChartLegend = true;
-  uvChartType = 'line';
-  uvChartPlugins = [];
-  uvChartLabels: Label[] = [];
-  uvChartOptions: ChartOptions = {
-    responsive: true
-  };
-  constructor(private forecastService: SevenDayForecastService, private authService: AuthService) { }
+  tempChart: Chart;
+  humidityChart: Chart;
+  uvChart: Chart;
+  favoriteLocations = [];
+
+  constructor(private forecastService: SevenDayForecastService, private authService: AuthService, private elementRef: ElementRef) { }
 
   async ngOnInit() {
     // Get the forecast from the service
     this.currentForecast = <Object[]> await this.forecastService.getForecast('29150');
     this.currentUVForecast = <Object[]> await this.forecastService.getUVForecast('29150');
+    this.fillTempChart(this.currentForecast);
+    this.fillHumidityChart(this.currentForecast);
+    this.fillUVChart(this.currentUVForecast);
+  }
+  // ------------------- Temp Chart ----------------------//
+  fillTempChart(currentForecast: any = {} as any){
+    let forcastTemps2 = [];
+    let forecastMaxTemps2 = [];
+    let forecastMinTemps2 = [];
+    let tempChartLabels2: Label[] = [];
     for (let i = 0; i < 40; i++){
-      this.forcastTemps[i] = this.currentForecast.list[i].main.temp;
-    }
-    for (let i = 0; i < 40; i++){
-      this.forecastMaxTemps[i] = this.currentForecast.list[i].main.temp_max;
-    }
-    for (let i = 0; i < 40; i++){
-      this.forecastMinTemps[i] = this.currentForecast.list[i].main.temp_min;
-    }
-    for (let i = 0; i < 40; i++){
+      forcastTemps2[i] = currentForecast.list[i].main.temp;
+      forecastMinTemps2[i] = currentForecast.list[i].main.temp_min;
+      forecastMaxTemps2[i] = currentForecast.list[i].main.temp_max;
       let str = this.currentForecast.list[i].dt_txt;
       let subStr = str.substring(5, str.length - 3);
-      this.tempChartLabels[i] = subStr;
+      tempChartLabels2[i] = subStr;
     }
-    for (let i = 0; i < 40; i++){
-      this.forcastHumidity[i] = this.currentForecast.list[i].main.humidity;
-    }
-    // ------------------- UV Chart ----------------------//
-    for (let i = 0; i < 8; i++){
-      this.uvChartLabels[i] = '+' + i;
-    }
-    for (let i = 0; i < 8; i++){
-      this.uvData[i] = this.currentUVForecast[i]?.value;
-    }
-
+    this.tempChart = new Chart('tempChart', {
+      type: 'line',
+      data: {
+          labels: tempChartLabels2,
+          datasets: [{
+            label: 'Temperature',
+            data: forcastTemps2,
+            backgroundColor: 'rgba(178, 174, 174, 0.2)',
+            borderColor: 'grey',
+            borderWidth: 2
+          },
+          {
+            label: 'Maximum Temperature',
+            data: forecastMaxTemps2,
+            backgroundColor: 'rgba(247, 105, 105, 0.2)',
+            borderColor: 'red',
+            borderWidth: 1
+          },
+          {
+            label: 'Minimum Temperature',
+            data: forecastMinTemps2,
+            backgroundColor: 'rgba(169, 218, 239, 0.2)',
+            borderColor: 'blue',
+            borderWidth: 1
+          }]
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          mode: 'index',
+          intersect: false
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
+        title: {
+          display: true,
+          text: '40 Hour Temperature Forecast'
+        }
+      }
+    });
   }
-
-  async switchLocations(zip: number){
-    this.currentForecast = <Object[]> await this.forecastService.getForecast(zip.toString());
-    this.currentUVForecast = <Object[]> await this.forecastService.getUVForecast(zip.toString());
+  // ------------------- Humidity Chart ----------------------//
+  fillHumidityChart(currentForecast: any = {} as any){
+    let humidityData = [];
+    let humidityLabels: Label[] = [];
     for (let i = 0; i < 40; i++){
-      this.forcastTemps[i] = this.currentForecast.list[i].main.temp;
-    }
-    for (let i = 0; i < 40; i++){
-      this.forecastMaxTemps[i] = this.currentForecast.list[i].main.temp_max;
-    }
-    for (let i = 0; i < 40; i++){
-      this.forecastMinTemps[i] = this.currentForecast.list[i].main.temp_min;
-    }
-    for (let i = 0; i < 40; i++){
+      humidityData[i] = currentForecast.list[i].main.humidity;
       let str = this.currentForecast.list[i].dt_txt;
       let subStr = str.substring(5, str.length - 3);
-      this.tempChartLabels[i] = subStr;
+      humidityLabels[i] = subStr;
     }
-    for (let i = 0; i < 40; i++){
-      this.forcastHumidity[i] = this.currentForecast.list[i].main.humidity;
-    }
-    // ------------------- UV Chart ----------------------//
-    for (let i = 0; i < 8; i++){
-      this.uvChartLabels[i] = '+' + i;
-    }
-    for (let i = 0; i < 8; i++){
-      this.uvData[i] = this.currentUVForecast[i]?.value;
-    }
+    this.tempChart = new Chart('humidityChart', {
+      type: 'line',
+      data: {
+          labels: humidityLabels,
+          datasets: [{
+            label: 'Humidity',
+            data: humidityData,
+            backgroundColor: 'rgba(21, 255, 255, 0.5)',
+            borderColor: 'black',
+            borderWidth: 2
+          }]
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          mode: 'index',
+          intersect: false
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
+        title: {
+          display: true,
+          text: '40 Hour Humidity Forecast'
+        }
+      }
+    });
   }
+  // ------------------- UV Chart ----------------------//
+  fillUVChart(currentUVForecast: any = {} as any){
+    let uvData = [];
+    let uvLabels: Label[] = [];
+    for (let i = 0; i < 8; i++){
+      uvData[i] = currentUVForecast[i]?.value;
+      uvLabels[i] = '' + i;
+    }
+    this.uvChart = new Chart('uvChart', {
+      type: 'line',
+      data: {
+          labels: uvLabels,
+          datasets: [{
+            label: 'UV Index',
+            data: uvData,
+            backgroundColor: 'rgba(255, 99, 71, 0.8)',
+            borderColor: 'black',
+            borderWidth: 2
+          }]
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          mode: 'index',
+          intersect: false
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
+        title: {
+          display: true,
+          text: '8 Hr UV Forecast'
+        }
+      }
+    });
+  }
+
 }
-
-
