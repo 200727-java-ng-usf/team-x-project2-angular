6b005d335722875321fb3076f6a9aa0061979da8
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-28 11:20:47 -0400

    created sunrise-sunset model

---
+++ src/app/models/sunrise-sunset.ts
@@ -0,0 +1,17 @@
+import { Time } from '@angular/common'
+
+export class SunriseSunset{
+  results: {
+    sunrise: Time;
+    sunset: Time;
+    solar_noon: Time;
+    day_length: Time;
+    civil_twilight_begin: Time;
+    civil_twilight_end: Time;
+    nautical_twilight_begin: Time;
+    nautical_twilight_end: Time;
+    astronomical_twilight_begi: Time;
+    astronomical_twilight_end: Time;
+  };
+  status: string;
+}
