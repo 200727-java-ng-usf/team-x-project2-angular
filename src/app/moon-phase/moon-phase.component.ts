212f1f66bb6f682afe04ae8846e06aef5eda097c
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-28 19:00:24 -0400

    auto generated moon phase component and service

---
+++ src/app/moon-phase/moon-phase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moon-phase',
+  templateUrl: './moon-phase.component.html',
+  styleUrls: ['./moon-phase.component.css']
+})
+export class MoonPhaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
