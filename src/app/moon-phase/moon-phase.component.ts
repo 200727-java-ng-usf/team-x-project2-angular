c1da132d1eea5c9b8feb9e1308aae529b58781b6
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-29 14:02:18 -0400

    done styling moon phase

--- src/app/moon-phase/moon-phase.component.ts
+++ src/app/moon-phase/moon-phase.component.ts
@@ -14,14 +14,17 @@ export class MoonPhaseComponent implements OnInit {
     phase: 0,
     angle: 0,
     date: '',
-    iconLink: ''
+    iconLink: {
+      friendlyName: '',
+      link: ''
+    }
   }];
   suncalc = SunCalc;
   constructor() { }
   today = new Date();
 
   ngOnInit(): void {
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 10; i++){
       let currentDay = new Date();
       currentDay.setTime( this.today.getTime() + (24 * i) * 60 * 60 * 1000);
       this.moonPhase[i] = this.suncalc.getMoonIllumination(currentDay);
@@ -33,34 +36,143 @@ export class MoonPhaseComponent implements OnInit {
   }
 
   getMoonImage(mooonFraction: number){
+    let icon = {
+      friendlyName: '',
+      link: ''
+    }
     if (mooonFraction < 0 ) { return; }
-    if (mooonFraction > 0  && mooonFraction < 1 / 28) { return 'wi wi-moon-new'; }
-    if (mooonFraction > 1 / 28 && mooonFraction < 2 / 28) { return 'wi wi-moon-waxing-crescent-1'; }
-    if (mooonFraction > 2 / 28 && mooonFraction < 3 / 28) { return 'wi wi-moon-waxing-crescent-2'; }
-    if (mooonFraction > 3 / 28 && mooonFraction < 4 / 28) { return 'wi wi-moon-waxing-crescent-3'; }
-    if (mooonFraction > 4 / 28 && mooonFraction < 5 / 28) { return 'wi wi-moon-waxing-crescent-4'; }
-    if (mooonFraction > 5 / 28 && mooonFraction < 6 / 28) { return 'wi wi-moon-waxing-crescent-5'; }
-    if (mooonFraction > 6 / 28 && mooonFraction < 7 / 28) { return 'wi wi-moon-waxing-crescent-6'; }
-    if (mooonFraction > 7 / 28 && mooonFraction < 8 / 28) { return 'wi wi-moon-first-quarter'; }
-    if (mooonFraction > 8 / 28 && mooonFraction < 9 / 28) { return 'wi wi-moon-waxing-gibbous-1'; }
-    if (mooonFraction > 9 / 28 && mooonFraction < 10 / 28) { return 'wi wi-moon-waxing-gibbous-2'; }
-    if (mooonFraction > 10 / 28 && mooonFraction < 11 / 28) { return 'wi wi-moon-waxing-gibbous-3'; }
-    if (mooonFraction > 11 / 28 && mooonFraction < 12 / 28) { return 'wi wi-moon-waxing-gibbous-4'; }
-    if (mooonFraction > 12 / 28 && mooonFraction < 13 / 28) { return 'wi wi-moon-waxing-gibbous-5'; }
-    if (mooonFraction > 13 / 28 && mooonFraction < 14 / 28) { return 'wi wi-moon-waxing-gibbous-6'; }
-    if (mooonFraction > 14 / 28 && mooonFraction < 15 / 28) { return 'wi wi-moon-full'; }
-    if (mooonFraction > 15 / 28 && mooonFraction < 16 / 28) { return 'wi wi-moon-waning-gibbous-1'; }
-    if (mooonFraction > 16 / 28 && mooonFraction < 17 / 28) { return 'wi wi-moon-waning-gibbous-2'; }
-    if (mooonFraction > 17 / 28 && mooonFraction < 18 / 28) { return 'wi wi-moon-waning-gibbous-3'; }
-    if (mooonFraction > 18 / 28 && mooonFraction < 19 / 28) { return 'wi wi-moon-waning-gibbous-4'; }
-    if (mooonFraction > 19 / 28 && mooonFraction < 20 / 28) { return 'wi wi-moon-waning-gibbous-5'; }
-    if (mooonFraction > 20 / 28 && mooonFraction < 21 / 28) { return 'wi wi-moon-waning-gibbous-6'; }
-    if (mooonFraction > 21 / 28 && mooonFraction < 22 / 28) { return 'wi wi-moon-third-quarter'; }
-    if (mooonFraction > 22 / 28 && mooonFraction < 23 / 28) { return 'wi wi-moon-waning-crescent-1'; }
-    if (mooonFraction > 23 / 28 && mooonFraction < 24 / 28) { return 'wi wi-moon-waning-crescent-2'; }
-    if (mooonFraction > 24 / 28 && mooonFraction < 25 / 28) { return 'wi wi-moon-waning-crescent-3'; }
-    if (mooonFraction > 25 / 28 && mooonFraction < 26 / 28) { return 'wi wi-moon-waning-crescent-4'; }
-    if (mooonFraction > 26 / 28 && mooonFraction < 27 / 28) { return 'wi wi-moon-waning-crescent-5'; }
-    if (mooonFraction > 27 / 28 && mooonFraction < 1) { return 'wi wi-moon-waning-crescent-6'; }
+    if (mooonFraction > 0  && mooonFraction < 1 / 28) {
+      icon.friendlyName = "New Moon";
+      icon.link = 'wi wi-moon-new';
+      return icon;
+    }
+    if (mooonFraction > 1 / 28 && mooonFraction < 2 / 28) {
+      icon.friendlyName = "Waxing Crescent 1";
+      icon.link = 'wi wi-moon-waxing-crescent-1';
+      return icon;
+    }
+    if (mooonFraction > 2 / 28 && mooonFraction < 3 / 28) {
+      icon.friendlyName = "Waxing Crescent 2";
+      icon.link = 'wi wi-moon-waxing-crescent-2';
+      return icon;
+    }
+    if (mooonFraction > 3 / 28 && mooonFraction < 4 / 28) {
+      icon.friendlyName = "Waxing Crescent 3";
+      icon.link = 'wi wi-moon-waxing-crescent-3';
+      return icon;
+      }
+    if (mooonFraction > 4 / 28 && mooonFraction < 5 / 28) {
+      icon.friendlyName = "Waxing Crescent 4";
+      icon.link = 'wi wi-moon-waxing-crescent-4';
+      return icon;
+    }
+    if (mooonFraction > 5 / 28 && mooonFraction < 6 / 28) {
+      icon.friendlyName = "Waxing Crescent 5";
+      icon.link = 'wi wi-moon-waxing-crescent-5';
+      return icon;
+      }
+    if (mooonFraction > 6 / 28 && mooonFraction < 7 / 28) {
+      icon.friendlyName = "Waxing Crescent 6";
+      icon.link = 'wi wi-moon-waxing-crescent-6';
+      return icon;
+    }
+    if (mooonFraction > 7 / 28 && mooonFraction < 8 / 28) {
+      icon.friendlyName = "First Quarter";
+      icon.link = 'wi wi-moon-first-quarter';
+      return icon;
+    }
+    if (mooonFraction > 8 / 28 && mooonFraction < 9 / 28) {
+      icon.friendlyName = "Waxing Gibbous 1";
+      icon.link = 'wi wi-moon-waxing-gibbous-1';
+      return icon;
+    }
+    if (mooonFraction > 9 / 28 && mooonFraction < 10 / 28) {
+      icon.friendlyName = "Waxing Gibbous 2";
+      icon.link = 'wi wi-moon-waxing-gibbous-2';
+      return icon;
+    }
+    if (mooonFraction > 10 / 28 && mooonFraction < 11 / 28) {
+      icon.friendlyName = "Waxing Gibbous 3";
+      icon.link = 'wi wi-moon-waxing-gibbous-3';
+      return icon;
+    }
+    if (mooonFraction > 11 / 28 && mooonFraction < 12 / 28) {
+      icon.friendlyName = "Waxing Gibbous 4";
+      icon.link = 'wi wi-moon-waxing-gibbous-4';
+      return icon;
+      }
+    if (mooonFraction > 12 / 28 && mooonFraction < 13 / 28) {
+      icon.friendlyName = "Waxing Gibbous 5";
+      icon.link = 'wi wi-moon-waxing-gibbous-5';
+      return icon;
+    }
+    if (mooonFraction > 13 / 28 && mooonFraction < 14 / 28) {
+      icon.friendlyName = "Waxing Gibbous 6";
+      icon.link = 'wi wi-moon-waxing-gibbous-6';
+      return icon;
+     }
+    if (mooonFraction > 14 / 28 && mooonFraction < 15 / 28) {
+      icon.friendlyName = "Full Moon";
+      icon.link = 'wi wi-moon-full';
+      return icon;
+    }
+    if (mooonFraction > 15 / 28 && mooonFraction < 16 / 28) {
+      icon.friendlyName = "Waning Gibbous 1";
+      icon.link = 'wi wi-moon-waning-gibbous-1';
+      return icon;
+    }
+    if (mooonFraction > 16 / 28 && mooonFraction < 17 / 28) {
+      icon.friendlyName = "Waning Gibbous 2";
+      icon.link = 'wi wi-moon-waning-gibbous-2';
+      return icon;
+    }
+    if (mooonFraction > 17 / 28 && mooonFraction < 18 / 28) {
+      icon.friendlyName = "Waning Gibbous 3";
+      icon.link = 'wi wi-moon-waning-gibbous-3';
+      return icon;
+    }
+    if (mooonFraction > 18 / 28 && mooonFraction < 19 / 28) {
+      icon.friendlyName = "Waning Gibbous 4";
+      icon.link = 'wi wi-moon-waning-gibbous-4';
+      return icon;
+      }
+    if (mooonFraction > 19 / 28 && mooonFraction < 20 / 28) {
+      icon.friendlyName = "Waning Gibbous 5";
+      icon.link = 'wi wi-moon-waning-gibbous-5';
+      return icon;      }
+    if (mooonFraction > 20 / 28 && mooonFraction < 21 / 28) {
+      icon.friendlyName = "Waning Gibbous 6";
+      icon.link = 'wi wi-moon-waning-gibbous-6';
+      return icon;      }
+    if (mooonFraction > 21 / 28 && mooonFraction < 22 / 28) {
+      icon.friendlyName = "Third Quarter";
+      icon.link = 'wi wi-moon-third-quarter';
+      return icon;
+      }
+    if (mooonFraction > 22 / 28 && mooonFraction < 23 / 28) {
+      icon.friendlyName = "Waning Crescent 1";
+      icon.link = 'wi wi-moon-waning-crescent-1';
+      return icon;
+      }
+    if (mooonFraction > 23 / 28 && mooonFraction < 24 / 28) {
+      icon.friendlyName = "Waning Crescent 2";
+      icon.link = 'wi wi-moon-waning-crescent-2';
+      return icon;    }
+    if (mooonFraction > 24 / 28 && mooonFraction < 25 / 28) {
+      icon.friendlyName = "Waning Crescent 3";
+      icon.link = 'wi wi-moon-waning-crescent-3';
+      return icon;      }
+    if (mooonFraction > 25 / 28 && mooonFraction < 26 / 28) {
+      icon.friendlyName = "Waning Crescent 4";
+      icon.link = 'wi wi-moon-waning-crescent-4';
+      return icon;    }
+    if (mooonFraction > 26 / 28 && mooonFraction < 27 / 28) {
+      icon.friendlyName = "Waning Crescent 5";
+      icon.link = 'wi wi-moon-waning-crescent-5';
+      return icon;    }
+    if (mooonFraction > 27 / 28 && mooonFraction < 1) {
+      icon.friendlyName = "Waning Crescent 6";
+      icon.link = 'wi wi-moon-waning-crescent-6';
+      return icon;      }
   }
 }
