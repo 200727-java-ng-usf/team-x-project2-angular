beffdbcbb30718591fd37dd1edd24982dab4d8f2
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-29 20:34:39 -0400

    added weather.gov api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,8 @@ export const environment = {
   PAST_WEATHER_API_URL: 'https://api.meteostat.net/v2/stations/',
   PAST_WEATHER_SEARCH_API_URL: 'https://api.meteostat.net/v2/stations/search?query=',
   PAST_WEATHER_API_KEY: 'vouELAGs6zgSdehEZb6emuihTEf32kPf', // Must be used in Header of Request
-  SUNRISE_SUNSET_API_URL: 'https://api.sunrise-sunset.org/json?' // NO api key needed
+  SUNRISE_SUNSET_API_URL: 'https://api.sunrise-sunset.org/json?', // NO api key needed
+  WEATHER_GOV_API_URL: 'https://api.weather.gov'
 };
 
 /*
