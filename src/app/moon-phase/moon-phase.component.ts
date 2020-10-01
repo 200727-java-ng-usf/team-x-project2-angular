9ff954824a6259893cee00418ac5c55c62f58761
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-30 20:37:08 -0400

    added auto search for home city to past weather

--- src/app/moon-phase/moon-phase.component.ts
+++ src/app/moon-phase/moon-phase.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as SunCalc from 'node_modules/suncalc/suncalc.js';
-
+import { AccountService } from '../services/account.service';
+import { Principal } from '../models/principal';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-moon-phase',
   templateUrl: './moon-phase.component.html',
@@ -26,7 +28,12 @@ export class MoonPhaseComponent implements OnInit {
     }
   }];
   suncalc = SunCalc;
-  constructor() { }
+  currentUserSubject: BehaviorSubject<Principal>;
+
+  constructor( private accountService: AccountService) {
+    this.currentUserSubject = accountService.getCurrentUserSubject();
+    console.log(this.currentUserSubject);
+   }
   today = new Date();
 
   ngOnInit(): void {
